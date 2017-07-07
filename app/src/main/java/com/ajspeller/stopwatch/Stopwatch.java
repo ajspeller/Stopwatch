22e16a9dd108aca1fc76eef65b8c38f75aa70b50
AJ Speller <ajspeller@gmail.com>
2017-07-07 18:16:23 -0400

    Initial Commit

---
+++ app/src/main/java/com/ajspeller/stopwatch/Stopwatch.java
@@ -0,0 +1,13 @@
+package com.ajspeller.stopwatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Stopwatch extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+}
